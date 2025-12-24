8ca8caaa5c196cff85b72e4e3371e9cc469c440d
Tom Cohen <tom.cohen03@post.runi.ac.il>
2025-12-24 16:46:12 +0200

    IntToBin implementation

--- IntToBin.java
+++ IntToBin.java
@@ -2,13 +2,27 @@
 public class IntToBin {
 
     public static void main(String[] args) {
+        //int x = 13;
         int x = Integer.parseInt(args[0]);
         System.out.println("Binary representation of " + x + " is: " + toBinary(x));
     }
 
     /** Returns the binary representation of the given integer, as a string of 0's and 1's.  */
     public static String toBinary(int x) {
-        //// Replace the following statement with your code
-        return "";
+        String s = "";
+        if (x == 1) return s = "1" + s;
+        if (x == 0) return s = "0" + s;
+
+        if (x >= 2)
+        {
+            if (x % 2 != 1 && x >= 0) s = "0";
+
+            if (x % 2 == 1 && x >= 1) s = "1";
+
+            x = x / 2;
+
+            return toBinary(x) + s;
+        }
+        return s;
     }    
  }
